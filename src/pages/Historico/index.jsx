cdbf28b6144dd8c6f5a42ba1cae5667d8ea6320a
João Stael <joaostael@gmail.com>
2021-08-04 11:05:06 -0300

    Ajuste no historico

--- src/pages/Historico/index.jsx
+++ src/pages/Historico/index.jsx
@@ -8,22 +8,19 @@ import { useEffect } from "react";
 import { Group, List } from "@material-ui/icons";
 import { ListGroupItem } from "reactstrap";
 
-
-
 function Historico() {
   const history = useHistory();
 
   const [recibos, setRecibos] = useState([]);
-    useEffect(() => {
-       http.get("http://localhost:8080/api/recibos")
-        .then(response=>{
-            const {data} = response;
-            setRecibos(data)
-        })
-        .catch(erro=>console.error(erro))
-    }, [])
-   
-    const impresso = recibos.map(recibos => <li className="List-group">{}</li> )    
+  useEffect(() => {
+    http
+      .get("http://localhost:8080/api/recibos")
+      .then((response) => {
+        const { data } = response;
+        setRecibos(data);
+      })
+      .catch((erro) => console.error(erro));
+  }, []);
 
   const abrirRecibo = () => {};
 
@@ -37,15 +34,16 @@ function Historico() {
             <h5 className="mb-0">Historico de Recibo</h5>
           </div>
           <ul className="list-group">
-            <li className="list-group-item">
-              <input
-                className="form-check-input me-1"
-                type="checkbox"
-                value=""
-                aria-label="..."
-              />
-              First checkbox
-            </li>
+            {recibos.map((recibo) => (
+              <li className="list-group-item">
+                <input
+                  className="form-check-input me-1"
+                  type="checkbox"
+                  value={recibo.nome}
+                  aria-label="..."
+                />
+              </li>
+            ))}
           </ul>
           <div className=" d-flex flex-row flex-wrap justify-content-center ">
             <div className="corpo-cadastro-procedimentos w-100"></div>
